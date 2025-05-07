e4091dc162d74ac74dd927b56be96ccea7f21393
Yernur Bekbolat <erkeerke349@gmail.com>
2025-05-07 16:21:29 +0500

    merge entities-branch to master branch

--- src/patterns/observer/GameObserver.java
+++ src/patterns/observer/GameObserver.java
@@ -1,4 +1,8 @@
 package patterns.observer;
 
+
 public interface GameObserver {
+    void update(String message);
+    void updateResources(int resources);
+    void updateState(boolean isRunning, boolean isPaused);
 }
