40c5c5ab4b4531a9661bed9a6f8c90c2f350037c
Yernur Bekbolat <erkeerke349@gmail.com>
2025-05-07 16:27:47 +0500

    merge logic-branch to master branch

--- src/patterns/strategy/BasicAttack.java
+++ src/patterns/strategy/BasicAttack.java
@@ -1,4 +1,20 @@
 package patterns.strategy;
+import entities.Entity;
+import gameobjects.Projectile;
+import java.awt.Color;
 
-public class BasicAttack  {
+public class BasicAttack implements AttackStrategy {
+    @Override
+    public void executeAttack(Entity attacker) {
+        int projStartX = attacker.getX() + attacker.getWidth();
+        int projStartY = attacker.getY() + attacker.getHeight() / 2 - 3;
+        Projectile projectile = new Projectile(
+                projStartX, projStartY,
+                8, 4,
+                5,
+                attacker.getAttackDamage(),
+                Color.YELLOW
+        );
+        attacker.getProjectiles().add(projectile);
+    }
 }
