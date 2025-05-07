2c48e72904b83bac0a23165117b2ac575cb617cf
Yernur Bekbolat <erkeerke349@gmail.com>
2025-05-07 16:18:39 +0500

    merge entities-branch to master branch

--- src/entities/HumanBase.java
+++ src/entities/HumanBase.java
@@ -1,4 +1,26 @@
 package entities;
 
-public class HumanBase {
+import java.awt.*;
+
+public class HumanBase extends Entity {
+    private static final int BASE_WIDTH = 80;
+    private static final int BASE_HEIGHT = 120;
+    private static final int BASE_X = 50;
+    private static final int BASE_Y = 240;
+
+    public HumanBase(int initialMaxHealth) {
+        super(BASE_X, BASE_Y, BASE_WIDTH, BASE_HEIGHT, initialMaxHealth, 0, 0, 0);
+    }
+
+    @Override protected void move() {}
+    @Override protected boolean canAttack() {
+        return false;
+    }
+    @Override protected void attack() {}
+    @Override protected Color getColor() {
+        return Color.GREEN.darker();
+    }
+    public boolean isDestroyed() {
+        return health <= 0;
+    }
 }
