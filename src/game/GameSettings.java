3ab5bfe158d16ba882bf40d8df5bb2be73b76fcc
Yernur Bekbolat <erkeerke349@gmail.com>
2025-05-07 16:23:42 +0500

    merge logic-branch to master branch

---
+++ src/game/GameSettings.java
@@ -0,0 +1,8 @@
+package game;
+
+public final class GameSettings {
+    public static final int TITAN_KILL_REWARD_BASE = 15;
+    public static final int WAVE_COMPLETE_REWARD_BASE = 75;
+    public static final int LEVEL_COMPLETE_REWARD_BASE = 150;
+    public static final int TITAN_BLOCKING_DISTANCE = 5;
+}
