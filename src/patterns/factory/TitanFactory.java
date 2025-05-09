22eb13c7d114c33387829a775d8c7e619b2ab526
Yernur Bekbolat <erkeerke349@gmail.com>
2025-05-09 15:19:35 +0500

    merge logic-branch to master branch

--- src/patterns/factory/TitanFactory.java
+++ src/patterns/factory/TitanFactory.java
@@ -1,4 +1,37 @@
 package patterns.factory;
 
-public class TitanFactory {
+import entities.Entity;
+import entities.EntityTypeData.TitanType;
+import entities.Titan;
+import game.Difficulty;
+
+public class TitanFactory implements EntityFactory {
+
+
+    @Override
+    public Entity createEntity(String typeKey_NotUsed, int x, int y, Difficulty difficulty) {
+        throw new UnsupportedOperationException("Use createTitan(TitanType, x, y, difficulty) instead for Titans.");
+    }
+
+
+    public Titan createTitan(TitanType type, int x, int y, Difficulty difficulty) {
+
+
+        Titan titan = new Titan(x, y,
+                type.getBaseHealth(),
+                type.getBaseDamage(),
+                type.getBaseRange(),
+                type.getBaseAttackSpeed(),
+                type.getBaseMoveSpeed());
+
+
+        titan.scaleMaxHealth(difficulty.getTitanHealthScale());
+        titan.setAttackDamage((int) Math.max(1, Math.round(titan.getAttackDamage() * difficulty.getTitanDamageScale())));
+        titan.setMovementSpeed((int) Math.max(1, Math.round(titan.getMovementSpeed() * difficulty.getTitanMoveSpeedScale())));
+
+
+        titan.setSprite(type.getSpritePath());
+
+        return titan;
+    }
 }
