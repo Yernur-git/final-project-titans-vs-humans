5aee4e132f1d50fdced32ddee25982ac6847d01a
Yernur Bekbolat <erkeerke349@gmail.com>
2025-05-09 15:12:47 +0500

    merge logic-branch to master branch

--- src/patterns/strategy/StrongAttack.java
+++ src/patterns/strategy/StrongAttack.java
@@ -1,4 +1,25 @@
 package patterns.strategy;
 
-public class StrongAttack  {
+import entities.Entity;
+import gameobjects.Projectile;
+
+import java.awt.*;
+
+
+public class StrongAttack implements AttackStrategy {
+    @Override
+    public void executeAttack(Entity attacker) {
+
+        Projectile projectile = new Projectile(
+                attacker,
+                attacker.getX() + attacker.getWidth(),
+                attacker.getY() + attacker.getHeight() / 2 - 5,
+                12, 8,
+                3,
+                attacker.getAttackDamage(),
+                attacker.getAttackRange(),
+                Color.RED
+        );
+        attacker.getProjectiles().add(projectile);
+    }
 }
