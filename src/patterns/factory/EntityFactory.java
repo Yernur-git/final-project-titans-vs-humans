9990bb2e86a4932a7c1592255bc6a13836580c53
Yernur Bekbolat <erkeerke349@gmail.com>
2025-05-09 15:18:46 +0500

    merge logic-branch to master branch

--- src/patterns/factory/EntityFactory.java
+++ src/patterns/factory/EntityFactory.java
@@ -1,4 +1,8 @@
 package patterns.factory;
 
+import entities.Entity;
+import game.Difficulty;
+
 public interface EntityFactory {
+    Entity createEntity(String type, int x, int y, Difficulty difficulty);
 }
