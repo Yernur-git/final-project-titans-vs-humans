ca8483b71f07b92b9a674bd8994285d6d19fa060
Yernur Bekbolat <erkeerke349@gmail.com>
2025-05-09 15:12:29 +0500

    merge logic-branch to master branch

--- src/patterns/strategy/FastAttack.java
+++ src/patterns/strategy/FastAttack.java
@@ -1,4 +1,24 @@
 package patterns.strategy;
 
-public class FastAttack {
+import entities.Entity;
+import gameobjects.Projectile;
+
+import java.awt.*;
+
+public class FastAttack implements AttackStrategy {
+    @Override
+    public void executeAttack(Entity attacker) {
+
+        Projectile projectile = new Projectile(
+                attacker,
+                attacker.getX() + attacker.getWidth(),
+                attacker.getY() + attacker.getHeight() / 2 - 2,
+                6, 3,
+                8,
+                attacker.getAttackDamage(),
+                attacker.getAttackRange(),
+                Color.CYAN
+        );
+        attacker.getProjectiles().add(projectile);
+    }
 }
