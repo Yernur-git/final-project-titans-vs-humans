dc43269bbf419c94f7b256e4664523820f2d2a1b
Yernur Bekbolat <erkeerke349@gmail.com>
2025-05-09 15:19:22 +0500

    merge logic-branch to master branch

--- src/patterns/factory/ObstacleFactory.java
+++ src/patterns/factory/ObstacleFactory.java
@@ -2,26 +2,42 @@ package patterns.factory;
 
 import entities.Entity;
 import entities.EntityTypeData.ObstacleType;
+import game.Difficulty;
+
 import java.lang.reflect.InvocationTargetException;
 
 public class ObstacleFactory implements EntityFactory {
+
+
     @Override
-    public Entity createEntity(String typeKey_NotUsed, int x, int y, double difficultyFactor) {
-        throw new UnsupportedOperationException("Use createObstacle(ObstacleType, x, y) instead.");
+    public Entity createEntity(String typeKey_NotUsed, int x, int y, Difficulty difficulty) {
+        throw new UnsupportedOperationException("Use createObstacle(ObstacleType, x, y) instead for Obstacles.");
     }
+
 
     public Entity createObstacle(ObstacleType type, int x, int y) {
         Entity obstacle = null;
         try {
-            obstacle = type.getEntityClass()
-                    .getDeclaredConstructor(int.class, int.class)
-                    .newInstance(x, y);
+
+            obstacle = type.getEntityClass().getDeclaredConstructor(int.class, int.class).newInstance(x, y);
+
+
             obstacle.setSprite(type.getSpritePath());
-        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            System.err.println("CRITICAL: Failed to instantiate obstacle: " + type.getDisplayName());
+
+
+            if (type == ObstacleType.TRAP && obstacle instanceof entities.SpikeTrap) {
+
+
+            }
+
+
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            System.err.println("CRITICAL: Failed to instantiate obstacle: " + type.getEntityClass().getName());
             e.printStackTrace();
             return null;
         }
+
         return obstacle;
     }
 }
