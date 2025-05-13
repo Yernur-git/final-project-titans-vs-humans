a5a946d97f1009290293628c645bd79905ec68b2
Merey-git <merei.kuanyshkhan@narxoz.kz>
2025-05-14 01:00:13 +0500

    Update HumanBase.java

--- src/entities/HumanBase.java
+++ src/entities/HumanBase.java
@@ -10,17 +10,57 @@ public class HumanBase extends Entity {
 
     public HumanBase(int initialMaxHealth) {
         super(BASE_X, BASE_Y, BASE_WIDTH, BASE_HEIGHT, initialMaxHealth, 0, 0, 0);
+        setSprite("humans/base.png");
     }
 
-    @Override protected void move() {}
-    @Override protected boolean canAttack() {
+    @Override
+    protected void move() {
+    }
+
+    @Override
+    protected boolean canAttack() {
         return false;
     }
-    @Override protected void attack() {}
-    @Override protected Color getColor() {
-        return Color.GREEN.darker();
+
+    @Override
+    protected void attack() {
     }
+
+    @Override
+    protected Color getColor() {
+        return new Color(0, 100, 0);
+    }
+
     public boolean isDestroyed() {
         return health <= 0;
+    }
+
+    @Override
+    protected void drawHealthBar(Graphics g) {
+        if (currentMaxHealth <= 0) return;
+
+        int barWidth = 150;
+        int barHeight = 10;
+        int barX = x + (width / 2) - (barWidth / 2);
+        int barY = y - barHeight - 10;
+
+        g.setColor(Color.RED);
+        g.fillRect(barX, barY, barWidth, barHeight);
+
+        double healthPercentage = (double) health / currentMaxHealth;
+        int greenWidth = (int) (barWidth * healthPercentage);
+        if (greenWidth < 0) greenWidth = 0;
+
+        g.setColor(Color.GREEN);
+        g.fillRect(barX, barY, greenWidth, barHeight);
+
+
+        g.setColor(Color.BLACK);
+        g.drawRect(barX, barY, barWidth, barHeight);
+    }
+
+    public void scaleBaseHealth(double factor) {
+        this.currentMaxHealth = (int) Math.max(1, Math.round(this.maxHealth * factor));
+        this.health = this.currentMaxHealth;
     }
 }
