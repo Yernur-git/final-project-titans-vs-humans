fcf35b979c50e89f9c1a7d973b7fbebe61b6e7fa
Merey-git <merei.kuanyshkhan@narxoz.kz>
2025-05-14 01:03:51 +0500

    Update ArmoredHuman.java

--- src/patterns/decorator/ArmoredHuman.java
+++ src/patterns/decorator/ArmoredHuman.java
@@ -1,33 +1,46 @@
 package patterns.decorator;
 
 import entities.Human;
+
 import java.awt.*;
 
 public class ArmoredHuman extends HumanDecorator {
-    private static final int ARMOR_COST_INCREASE = 30;  
-    private static final double DAMAGE_REDUCTION = 0.70; 
+    private static final int ARMOR_COST_INCREASE = 30;
+    private static final double DAMAGE_REDUCTION_FACTOR = 0.70;
 
     public ArmoredHuman(Human decoratedHuman) {
         super(decoratedHuman);
+
     }
 
     @Override
     public void takeDamage(int damage) {
-        int reducedDamage = (int) (damage * DAMAGE_REDUCTION);
-        super.takeDamage(reducedDamage); 
+
+        int reducedDamage = (int) (damage * DAMAGE_REDUCTION_FACTOR);
+        super.takeDamage(reducedDamage);
     }
 
     @Override
     public int getCost() {
+
+
         return decoratedHuman.getCost() + ARMOR_COST_INCREASE;
     }
 
     @Override
     public void draw(Graphics g) {
         super.draw(g);
+
+
         Graphics2D g2d = (Graphics2D) g.create();
-        g2d.setColor(new Color(0, 0, 255, 60)); 
+        g2d.setColor(new Color(0, 0, 255, 60));
         g2d.fillRect(getX(), getY(), getWidth(), getHeight());
         g2d.dispose();
+    }
+
+    @Override
+    public Color getColor() {
+
+        return new Color(100, 100, 255);
     }
 }
