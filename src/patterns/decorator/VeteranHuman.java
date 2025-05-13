20748ae3458e32e86d3cc29d3ffe6efe5ba62f39
Merey-git <merei.kuanyshkhan@narxoz.kz>
2025-05-14 01:05:18 +0500

    Update VeteranHuman.java

--- src/patterns/decorator/VeteranHuman.java
+++ src/patterns/decorator/VeteranHuman.java
@@ -1,16 +1,18 @@
 package patterns.decorator;
 
 import entities.Human;
+
 import java.awt.*;
 
 public class VeteranHuman extends HumanDecorator {
-    private static final int VETERAN_COST_INCREASE = 50; 
-    private static final double DAMAGE_MULTIPLIER = 1.25; 
-
+    private static final int VETERAN_COST_INCREASE = 50;
+    private static final double ATTACK_DAMAGE_MULTIPLIER = 1.25;
+    
     public VeteranHuman(Human decoratedHuman) {
         super(decoratedHuman);
-        int boostedDamage = (int) (decoratedHuman.getAttackDamage() * DAMAGE_MULTIPLIER);
-        this.setAttackDamage(boostedDamage); 
+
+        int boostedDamage = (int) (decoratedHuman.getAttackDamage() * ATTACK_DAMAGE_MULTIPLIER);
+        this.setAttackDamage(boostedDamage);
     }
 
     @Override
@@ -23,8 +25,14 @@ public class VeteranHuman extends HumanDecorator {
         super.draw(g);
         Graphics2D g2d = (Graphics2D) g.create();
         g2d.setColor(Color.ORANGE);
-        g2d.setStroke(new BasicStroke(2)); 
-        g2d.drawRect(getX() - 1, getY() - 1, getWidth() + 2, getHeight() + 2); 
+        g2d.setStroke(new BasicStroke(2));
+        g2d.drawRect(getX() - 1, getY() - 1, getWidth() + 1, getHeight() + 1);
         g2d.dispose();
+    }
+
+    @Override
+    public Color getColor() {
+
+        return new Color(150, 50, 150);
     }
 }
